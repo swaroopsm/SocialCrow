7d01921ca38187db32e2d4938101671848bd9e94
swaroop <swaroop.striker@gmail.com>
2012-05-07 09:31:48 +0530

    added initial files and directories

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialCrow::Application.config.secret_token = '981b7a730020c0ccef29d157adbf2229fb071326e895ac44c3b70677ebbdf7d04c6134472cadebbbc759eec820680b25f0ce7361bf0606215e14bef9d01eff3d'
