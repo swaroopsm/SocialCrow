ffc413fa1036e1ffdb19d48ae02f61960c8860ff
swaroop <swaroop.striker@gmail.com>
2012-05-08 20:45:03 +0530

    users relation added

--- db/schema.rb
+++ db/schema.rb
@@ -10,3 +10,14 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20120508150939) do
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+end
