106954cda109f1fac52ead5bc21f2b64d0ebf7e1
swaroop <swaroop.striker@gmail.com>
2012-05-08 23:16:20 +0530

    add column password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120508150939) do
+ActiveRecord::Schema.define(:version => 20120508171626) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
