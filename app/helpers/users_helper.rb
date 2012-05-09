1c5bf305b63e57d8f8b911f3e08265fce93028f1
swaroop <swaroop.striker@gmail.com>
2012-05-09 10:44:53 +0530

    linked image to user profiles page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,6 +2,6 @@ module UsersHelper
 	def gravatar_for(user)
 		gravatar_id=Digest::MD5::hexdigest(user.email.downcase)
 		gravatar_url="http://gravatar.com/avatar/#{gravatar_id}.png"
-		image_tag(gravatar_url,alt:user.name,class:"",title:user.name)
+		link_to(image_tag(gravatar_url,alt:user.name,class:"",title:user.name),"/@/#{user.id}")
 	end
 end
