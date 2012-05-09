12f84218004b376af33466e9916d8e09d7036a44
swaroop <swaroop.striker@gmail.com>
2012-05-09 22:21:15 +0530

    added create method and an instamce variable in new method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
   def new
+  	@user=User.new
   end
   
   def show
   	@user=User.find_by_username(params[:username])
+  end
+  
+  def create
+  	@user=User.new(params[:user])
+  	a=params[:username]
+  	if @user.save
+  		 redirect_to "/@/#{params[:user][:username]}"
+  	else
+  		render 'new'
+  	end
   end
 end
