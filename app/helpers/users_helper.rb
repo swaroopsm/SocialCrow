43db6e24568a8ae007250e9fb646023bfffe72bb
swaroop <swaroop.striker@gmail.com>
2012-05-09 19:26:09 +0530

    linked username to image instead of id

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,6 +2,6 @@ module UsersHelper
 	def gravatar_for(user)
 		gravatar_id=Digest::MD5::hexdigest(user.email.downcase)
 		gravatar_url="http://gravatar.com/avatar/#{gravatar_id}.png"
-		link_to(image_tag(gravatar_url,alt:user.name,class:"gravatar",title:user.name),"/@/#{user.id}")
+		link_to(image_tag(gravatar_url,alt:user.name,class:"gravatar",title:user.name),"/@/#{user.username}")
 	end
 end
