64e062723f11ea3b9c86a95ae2349d3c53f3f87b
swaroop <swaroop.striker@gmail.com>
2012-05-10 00:32:22 +0530

    authenticate for user, if not back to login page with flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,16 @@
 class SessionsController < ApplicationController
 	def new
-	
+		
 	end
 	
 	def create
-	
+		user=User.find_by_username(params[:session][:username])
+		if user && user.authenticate(params[:session][:password])
+			redirect_to "/@/#{params[:session][:username]}"
+		else
+			flash[:error] = "Login failed! Please try again!"
+			redirect_to "/login"
+		end
 	end
 	
 	def destroy
