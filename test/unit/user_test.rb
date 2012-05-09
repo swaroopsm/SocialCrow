7b539190e4dfe07a55869d03adb29c94befa9878
swaroop <swaroop.striker@gmail.com>
2012-05-09 19:15:55 +0530

    username annotation

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -8,6 +8,7 @@
 #  created_at      :datetime        not null
 #  updated_at      :datetime        not null
 #  password_digest :string(255)
+#  username        :string(255)
 #
 
 require 'test_helper'
