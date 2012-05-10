2442401289e6cb478839c3a8d9bb1fabd86fea5b
swaroop <swaroop.striker@gmail.com>
2012-05-10 15:57:17 +0530

    added remember_token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120509132555) do
+ActiveRecord::Schema.define(:version => 20120510102404) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(:version => 20120509132555) do
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
     t.string   "username"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
