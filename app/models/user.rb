be3598f6c9e6c7484edbcd12932ca9a4904562f0
swaroop <swaroop.striker@gmail.com>
2012-05-11 18:53:44 +0530

    create token during user create and a base64 genration of remember_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,9 +19,13 @@ class User < ActiveRecord::Base
 	attr_accessible :name, :email, :password, :password_confirmation, :username
   has_secure_password
   
+  
+  before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
+  
   private
   
   	def create_remember_token
-  		self.remember_token=SecureRandom.urlsafe_base64
+  	 self.remember_token = SecureRandom.urlsafe_base64
   	end
 end
