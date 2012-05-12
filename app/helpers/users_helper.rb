f4d2d00bc464182e5b2fe1be2bdf7ec80d203c42
swaroop <swaroop.striker@gmail.com>
2012-05-12 14:23:36 +0530

    specified size as an arguement for gravatar url and method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
-	def gravatar_for(user)
+	def gravatar_for(user,size=100)
 		gravatar_id=Digest::MD5::hexdigest(user.email.downcase)
-		gravatar_url="http://gravatar.com/avatar/#{gravatar_id}.png"
+		gravatar_url="http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
 		link_to(image_tag(gravatar_url,alt:user.name,class:"gravatar",title:user.name),"/@/#{user.username}")
 	end
 end
