ef96ed92383be4cd61f31b5856495ac82d4de523
swaroop <swaroop.striker@gmail.com>
2012-05-14 17:06:45 +0530

    added redirect_back_to call

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 		user=User.find_by_username(params[:session][:username])
 		if user && user.authenticate(params[:session][:password])
 			sign_in user
-			redirect_to "/@/#{params[:session][:username]}"
+			redirect_back_to
 		else
 			flash.now[:error] = "Login failed! Please try again!"
 			render 'new'
